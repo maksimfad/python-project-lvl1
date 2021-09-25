03febdd5acdb1f6a926d469785106cff27ba9834
Максим Фадеев <maksimfadeev@MacBook-Air-Maksim.local>
2021-09-25 21:56:39 +0300

    stage 4 part 1

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,9 @@
+"""This is module where welcome_user function defined."""
+
 import prompt
 
+
 def welcome_user():
-	name = prompt.string('May I have your name? ')
-	print('Hello, {}'.format(name))
-
-
+    """Use this funciton to get user name and print greeting with it."""
+    name = prompt.string('May I have your name? ')
+    print('Hello, {0}'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
+"""This module is script that do all the stuff."""
+
 from brain_games.cli import welcome_user
 
+
 def main():
-	print('Welcome to the Brain Games!')
-	welcome_user()
+    """Use this function to welcome and to greet user."""
+    print('Welcome to the Brain Games!')
+    welcome_user()
+
 
 if __name__ == '__main__':
-	main()
+    main()
