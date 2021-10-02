0c7fd22ba4e3fcd0b99f4f8d97c1d83915cbee9a
maksimfadeev <7522688@gmail.com>
2021-10-02 20:01:24 +0300

    stage 7

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 [![Actions Status](https://github.com/maksimfad/python-project-lvl1/workflows/Python%20CI/badge.svg)](https://github.com/maksimfad/python-project-lvl1/actions)
 [![asciicast](https://asciinema.org/a/WDM587IVFAtqxu4gBX6yaSN64.svg)](https://asciinema.org/a/WDM587IVFAtqxu4gBX6yaSN64)
 [![asciicast](https://asciinema.org/a/RCfZe61Je8Vs53nVYdwxyXVfV.svg)](https://asciinema.org/a/RCfZe61Je8Vs53nVYdwxyXVfV)
-
+[![asciicast](https://asciinema.org/a/YZvZR6D5ZuJTlXPj8LNKiUPKi.svg)](https://asciinema.org/a/YZvZR6D5ZuJTlXPj8LNKiUPKi)
---
+++ brain_games/games/gcd_game.py
@@ -0,0 +1,25 @@
+"""This is module where gcd game logic defined."""
+
+from random import randint  # isort:skip
+
+
+description_of_game = 'Find the greatest common divisor of given numbers.'
+
+
+def game_question_and_answer():
+    """Use this function to generate question and right answer and returns them."""
+    first_number = randint(1, 100)
+    second_number = randint(1, 100)
+    question = '{0} {1}'.format(first_number, second_number)
+    if first_number < second_number:
+        min_number = first_number
+    else:
+        min_number = second_number
+    test_divisor = 1
+    max_divisor = 1
+    while test_divisor <= min_number:
+        if (first_number % test_divisor == 0) & (second_number % test_divisor == 0):
+            max_divisor = test_divisor
+        test_divisor += 1
+    right_answer = str(max_divisor)
+    return question, right_answer
